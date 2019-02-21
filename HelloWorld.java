da57ab011cc44859687446338b38649b5be04a9a
TrnSharma8866 <trnsharma8866@gmail.com>
2019-02-21 10:43:49 +0530

    Helloworld program

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package com.reg.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				System.out.println("please enter a digit");
+				
+		}
+	
+
+}
