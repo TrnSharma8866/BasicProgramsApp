368c7ae67e915f6e650a366bc5cdef952f9b743d
TrnSharma8866 <trnsharma8866@gmail.com>
2019-02-21 10:50:24 +0530

    Program to display an array from Middle to Right

---
+++ ArrayMiddleToRightDisplay.java
@@ -0,0 +1,14 @@
+package com.reg.java;
+
+public class ArrayMiddleToRightDisplay {
+public static void main(String args[]) {
+        
+		int arr[]={10,20,30,40,50,60,70,80};
+		
+		for(int i=4;i<arr.length;i++)
+		{	
+		System.out.println(arr[i]);	
+		}	
+	}
+
+}
